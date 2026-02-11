1957e9a0439822b307cf0b525d0c61d0181ead3a
Adeniyi David <bemintdav@gmail.com>
2026-02-11 22:48:10 +0100

    Added My first django app to list of installed apps

--- First_Web_App/First_Web_App/settings.py
+++ First_Web_App/First_Web_App/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'My_First_Django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
