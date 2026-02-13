9d232d43bb9d7a89789bdd5f601449b48b3aaacb
Adeniyi David <bemintdav@gmail.com>
2026-02-13 13:28:00 +0100

    Made changes to folder names and added valentine to settings.py

--- First_Web_App/My_First_Django/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyFirstDjangoConfig(AppConfig):
-    name = 'My_First_Django'
--- First_Web_Rep/Django_Core/settings.py
+++ First_Web_Rep/Django_Core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'valentine',
     'My_First_Django',
     'django.contrib.admin',
     'django.contrib.auth',
